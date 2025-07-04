3a587d42b8166f8fd46344a0f6ae7f70257f2987
Chathan Driehuys <chathan@driehuys.com>
2025-07-04 12:53:45 +0000

    feat: require login

--- oral_prep/quiz/views.py
+++ oral_prep/quiz/views.py
@@ -1,8 +1,10 @@
 import random
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from quiz import models
 
+@login_required
 def home_view(req):
     question_count = models.Question.objects.count()
     question = models.Question.objects.all()[random.randint(0, question_count-1)]
